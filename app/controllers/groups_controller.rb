9965869e25362734a2b021a655e832453015b973
Kingstalux <kingsleyngu25@gmail.com>
2022-03-14 13:32:09 +0100

    Add create category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,5 +8,21 @@ class GroupsController < ActionController::Base
     end
 
     def create
+        group = current_user.groups.new(group_params)
+        respond_to do |format|
+            format.html do
+                if group.save
+                    redirect_to groups_path
+                else
+                    render :new
+                end
+            end 
+        end
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:name, :icon)
     end
 end
