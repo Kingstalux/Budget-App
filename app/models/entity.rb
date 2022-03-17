7e54c5748451bcdf15baac9da06fb5781ac02d0e
Kingstalux <kingsleyngu25@gmail.com>
2022-03-17 07:20:13 +0100

    Add entity styles

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,4 +3,8 @@ class Entity < ApplicationRecord
   has_many :relations, dependent: :destroy
   validates :name, presence: true
   validates :amount, numericality: { greater_than: 0 }
+
+  def created_at
+    attributes['created_at'].strftime('%d %b %Y')
+  end
 end
