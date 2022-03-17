db159aa7693e93c2d90b5455f8ea90dc9bba19eb
Kingstalux <kingsleyngu25@gmail.com>
2022-03-17 10:02:08 +0100

    Fix linters

--- spec/integration/group_index_spec.rb
+++ spec/integration/group_index_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Group Index', type: :feature do
   describe 'Group' do
-
     before(:each) do
-    @user1 = User.create! name: 'Justin', password: '000000', email: 'justin@gmail.com'
-    visit new_user_session_path
-    fill_in('Email', with: 'justin@gmail.com')
-    fill_in('Password', with: '000000')
-    click_button('LOG IN')
+      @user1 = User.create! name: 'Justin', password: '000000', email: 'justin@gmail.com'
+      @group = @user1.groups.create! name: 'HBO', icon: 'icon'
+      visit new_user_session_path
+      fill_in('Email', with: 'justin@gmail.com')
+      fill_in('Password', with: '000000')
+      click_button('LOG IN')
       visit root_path
     end
 
@@ -20,5 +20,8 @@ RSpec.describe 'Group Index', type: :feature do
       expect(page).to have_selector(:link_or_button, 'Categories')
     end
 
+    it 'has name and delete button of new group' do
+      expect(page).to have_selector(:link_or_button, 'HBO')
+    end
   end
 end
--- spec/integration/group_show_spec.rb
+++ spec/integration/group_show_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Group Index', type: :feature do
   describe 'Group' do
-
     before(:each) do
-    @user1 = User.create! name: 'Justin', password: '000000', email: 'justin@gmail.com'
-    @group = @user1.groups.create! name: 'HBO', icon: 'icon'
-    visit new_user_session_path
-    fill_in('Email', with: 'justin@gmail.com')
-    fill_in('Password', with: '000000')
-    click_button('LOG IN')
-    visit root_path
-    click_on('HBO')
+      @user1 = User.create! name: 'Justin', password: '000000', email: 'justin@gmail.com'
+      @group = @user1.groups.create! name: 'HBO', icon: 'icon'
+      visit new_user_session_path
+      fill_in('Email', with: 'justin@gmail.com')
+      fill_in('Password', with: '000000')
+      click_button('LOG IN')
+      visit root_path
+      click_on('HBO')
     end
 
     it 'has some buttons' do
@@ -22,6 +21,5 @@ RSpec.describe 'Group Index', type: :feature do
     it 'has heading in navbar' do
       expect(page).to have_selector(:link_or_button, 'Transactions')
     end
-
   end
 end
