30865618d69ee5859cb50acfea6eb14bd92ac720
Kingstalux <kingsleyngu25@gmail.com>
2022-03-17 09:46:42 +0100

    Add group index tests

---
+++ spec/integration/group_index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Group Index', type: :feature do
+  describe 'Group' do
+
+    before(:each) do
+    @user1 = User.create! name: 'Justin', password: '000000', email: 'justin@gmail.com'
+    visit new_user_session_path
+    fill_in('Email', with: 'justin@gmail.com')
+    fill_in('Password', with: '000000')
+    click_button('LOG IN')
+      visit root_path
+    end
+
+    it 'has button to add new group' do
+      expect(page).to have_selector(:link_or_button, 'Add Category')
+    end
+
+    it 'has heading in navbar' do
+      expect(page).to have_selector(:link_or_button, 'Categories')
+    end
+
+  end
+end
